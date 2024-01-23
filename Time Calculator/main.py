93690e9f613d72b035b24fbe96c58db9036e94ad
Juan Manuel Tellez Perez <45768562+juanM20@users.noreply.github.com>
2024-01-23 12:03:57 -0600

    first step in time calculator

---
+++ Time Calculator/main.py
@@ -0,0 +1,8 @@
+from time_calculator import add_time 
+
+
+def main():
+    print(add_time("3:00 PM", "3:10"))
+
+if __name__ == "__main__":
+    main()
---
+++ Time Calculator/time_calculator.py
@@ -0,0 +1,5 @@
+
+def add_time(time, time2):
+  
+  result = time + time2
+  return result
