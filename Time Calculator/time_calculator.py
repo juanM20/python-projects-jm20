160b130cf451c52fcbc4c35f813d43a0b9aebbdc
Juan Manuel Tellez Perez <45768562+juanM20@users.noreply.github.com>
2024-01-23 18:10:54 -0600

    split strings
    Split string in order to get the numbers of the hours and the minutes  from the time 1 and time 3

--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -1,5 +1,14 @@
 
 def add_time(time, time2):
   
+
+
+  print(time.find(' '), time2.find(':'))
+
+  time1 = time.split(' ')
+  day = time1[1]
+  [t1_h, t1_m] = time1[0].split(':') 
+  [t2_h, t2_m] = time2.split(':')
+  print(t1_h, t1_m, t2_h, t2_m)
   result = time + time2
   return result
