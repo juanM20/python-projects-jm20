aa1aaff150799684104c247e048706a057a7cfd8
Juan Manuel Tellez Perez <45768562+juanM20@users.noreply.github.com>
2024-01-24 00:20:38 -0600

    time_calculator

--- Time Calculator/main.py
+++ Time Calculator/main.py
@@ -2,7 +2,9 @@ from time_calculator import add_time
 
 
 def main():
-    print(add_time("3:00 PM", "3:10"))
+    # print(add_time("3:00 PM", "3:10"))
+    print(add_time("11:30 AM", "2:32", "Monday"))
+    
 
 if __name__ == "__main__":
     main()
